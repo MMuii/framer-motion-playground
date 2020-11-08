bb21de22024d5d2f645b349f018dd6392be05266
Marcin Świderek <muimuikrol@gmail.com>
2020-11-09 00:53:15 +0100

    Commented out bugged code

--- src/pages/tutorial.js
+++ src/pages/tutorial.js
@@ -6,48 +6,52 @@ import FullPageContainer from '../components/FullPageContainer';
 import { data } from '../tutorials-data/switch-button';
 
 const Tutorial = props => {
-    const { files, content } = data;
-    const [selectedFile, setSelectedFile] = useState(files[0]);
-
-    const renderFiles = () => {
-        return files.map(({id, name, extension}, index) => {
-            const isSelected = selectedFile.id === id ? 'selected' : '';
-
-            return (
-                <div 
-                    key={index} 
-                    className={`code__file code__file--${extension} ${isSelected}`}
-                    onClick={() => setSelectedFile(files[index])}
-                >
-                    {name}<span>.{extension}</span>
-                </div>
-            )
-        });
-    }
-    
     return (
-        <FullPageContainer className="tutorial">
-            <div className="guide guide__wrapper">
-                <div className="guide__content">
-                    {content}
-                </div>
-            </div>
-            <div className="code__wrapper">
-                <div className="code__navbar">
-                    <span>Completed code</span>
-                    {renderFiles()}
-                </div>
-                <SyntaxHighlighter 
-                    language={selectedFile.language}
-                    style={atomOneDark} 
-                    showLineNumbers
-                    className="code__content"
-                >
-                    {selectedFile.code}
-                </SyntaxHighlighter>
-            </div>
-        </FullPageContainer>
-    )
+        <div>strona do wywalenia</div>
+    );
+
+    // const { files, content } = data;
+    // const [selectedFile, setSelectedFile] = useState(files[0]);
+
+    // const renderFiles = () => {
+    //     return files.map(({id, name, extension}, index) => {
+    //         const isSelected = selectedFile.id === id ? 'selected' : '';
+
+    //         return (
+    //             <div 
+    //                 key={index} 
+    //                 className={`code__file code__file--${extension} ${isSelected}`}
+    //                 onClick={() => setSelectedFile(files[index])}
+    //             >
+    //                 {name}<span>.{extension}</span>
+    //             </div>
+    //         )
+    //     });
+    // }
+    
+    // return (
+    //     <FullPageContainer className="tutorial">
+    //         <div className="guide guide__wrapper">
+    //             <div className="guide__content">
+    //                 {content}
+    //             </div>
+    //         </div>
+    //         <div className="code__wrapper">
+    //             <div className="code__navbar">
+    //                 <span>Completed code</span>
+    //                 {renderFiles()}
+    //             </div>
+    //             <SyntaxHighlighter 
+    //                 language={selectedFile.language}
+    //                 style={atomOneDark} 
+    //                 showLineNumbers
+    //                 className="code__content"
+    //             >
+    //                 {selectedFile.code}
+    //             </SyntaxHighlighter>
+    //         </div>
+    //     </FullPageContainer>
+    // )
 }
 
 export default Tutorial;
