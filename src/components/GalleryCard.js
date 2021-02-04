071a982275f59aa3c7e506b46e3dd590b7a5eb5b
Marcin Świderek <muimuikrol@gmail.com>
2021-02-04 02:51:38 +0100

    Finished new hover effect

--- src/components/GalleryCard.js
+++ src/components/GalleryCard.js
@@ -1,26 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const GalleryCard = props => {
-    const [hovering, setHovering] = useState(false);
-    const { children, className, noOverflow, tutorial, ...restOfProps} = props;
+    const {
+        children,
+        tutorial, 
+        tutorial: { className, slug, name, description, direction },
+        hovering,
+        setHovering
+    } = props;
+
+    const [hoveringLink, setHoveringLink] = useState(false);
+    const [hoverable, setHoverable] = useState(true);
+
+    const variants = {
+        initial: {
+            x: direction === 'left' ? -15 : 15,
+            opacity: 0
+        },
+        animate: {
+            x: 0,
+            opacity: 1,
+            transition: {
+                staggerChildren: .07
+            }
+        },
+        exit: {
+            x: direction === 'left' ? -15 : 15,
+            opacity: 0,
+            transition: {
+                staggerChildren: .07
+            }
+        }
+    }
+
+    const linkHover = direction === 'left' 
+        ? { paddingRight: '30px' }
+        : { paddingLeft: '30px' };
 
     return (
         <div 
-            className={`gallery-card container ${className || ''}`} {...restOfProps}
-            onMouseEnter={() => setHovering(true)}
-            onMouseLeave={() => setHovering(false)}
-            style={{ zIndex: hovering ? 10 : 1 }}
+            className={`gallery-card container ${className || ''}`} 
+            onMouseEnter={() => setHovering(tutorial)}
+            onMouseLeave={() => {
+                setHovering(false);
+                setHoverable(false);
+            }}
+            style={{ zIndex: hovering ? 10 : 1, pointerEvents: hoverable ? 'all' : 'none' }}
         >
             <div className="gallery-card__content-wrapper">
                 {children}
             </div>
-            <div className="gallery-card__buttons-wrapper">
-                <Link to={`/tutorial/${tutorial}`}>
-                    <span>Tutorial</span> 
-                    <i className="fas fa-code"/>
-                </Link>
-            </div>
+
+            <AnimatePresence onExitComplete={() => setHoverable(true)}>
+                {hovering && (
+                    <motion.div 
+                        className={`gallery-card__description-wrapper gallery-card__description-wrapper--${direction}`}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        variants={variants}
+                    >
+                        <motion.h1 variants={variants}>{name}</motion.h1>
+                        <motion.p variants={variants}>{description}</motion.p>
+                        <Link to={`/tutorial/${slug}`} >
+                            <motion.div
+                                whileHover={linkHover}
+                                onMouseEnter={() => setHoveringLink(true)}
+                                onMouseLeave={() => setHoveringLink(false)}
+                            >
+                                <motion.div variants={variants}>See tutorial</motion.div>
+                                {hoveringLink && (
+                                    <motion.i 
+                                        className="fas fa-chevron-right"
+                                        initial={{ x: direction === 'left' ? 15 : -30, opacity: 0 }}
+                                        animate={{ x: direction === 'left' ? 15 : -30, opacity: 1 }}
+                                        exit={{ x: direction === 'left' ? 15 : -30, opacity: 0 }}
+                                    />
+                                )}
+                            </motion.div>
+                        </Link>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'gatsby';
+import { motion } from 'framer-motion';
+import { use100vh } from 'react-div-100vh';
 import GalleryCard from '../components/GalleryCard';
 import Switch from '../components/Switch';
 import ClipPathTransition from '../components/ClipPathTransition';
@@ -10,50 +12,116 @@ import UnderlinedMenu from '../components/underlined-menu/UnderlinedMenu';
 import RotatingCard from '../components/rotating-card/RotatingCard';
 import IphoneAnimation from '../components/iphone-animation/IphoneAnimation';
 
-const IndexPage = () => (
-    <div className="gallery">
-        <div className="gallery__info">
-            <h1>framer motion<br />playground</h1>
-            <button><Link to="/tutorial">about</Link></button>
-        </div>
-        <div className="gallery__container">
-            <GalleryCard tutorial="switch-button">
-                <Switch />
-            </GalleryCard>
+const tutorials = [
+    {
+        slug: 'switch-button',
+        name: 'Switch button',
+        description: 'Simple switch button, powered by layout animations.',
+        className: 'c1 r1',
+        component: <Switch />,
+        direction: 'left'
+    },
+    {
+        slug: 'infinite-cards',
+        name: 'Infinite cards',
+        description: 'You can swipe them all day long and they never end!',
+        className: 'c1 r1',
+        component: <InfiniteCards />,
+        direction: 'right'
+    },
+    {
+        slug: '3d-text',
+        name: '3D text effect',
+        description: 'You can achieve much more with CSS box shadow than you\'d think!',
+        className: 'c3 r2',
+        component: <Text3d />,
+        direction: 'right'
+    },
+    {
+        slug: 'wave',
+        name: 'Text wave',
+        description: 'You didn\'t hovered it just once, did you? // TODO',
+        className: 'c2 r1',
+        component: 'Wave',
+        direction: 'left'
+    },
+    {
+        slug: 'underlined-menu',
+        name: 'Underlined Menu',
+        description: 'Great example of very basic AnimateSharedLayout usage.',
+        className: 'c4 r1',
+        component: <UnderlinedMenu />,
+        direction: 'left'
+    },
+    {
+        slug: 'custom-cursor',
+        name: 'Custom cursor',
+        description: 'Even though it\'s Framer Motion Playground, this cursor is pure, plain CSS!',
+        className: 'c2 r2',
+        component: <CustomCursor />,
+        direction: 'left'
+    },
+    {
+        slug: 'clip-path-transition',
+        name: 'Clip Path Transition',
+        description: 'Nice transition made possible with animating clip path. Same mechanism as this site\'s page transition.',
+        className: 'c3 r2',
+        component: <ClipPathTransition />,
+        direction: 'right'
+    },
+    {
+        slug: 'rotating-card',
+        name: 'Rotating Card',
+        description: 'There is less math than you would think!',
+        className: 'c2 r3',
+        component: <RotatingCard />,
+        direction: 'left'
+    },
+    {
+        slug: 'app-open',
+        name: 'App opening animation',
+        description: 'AnimateSharedLayout can do magic in just few lines of code. Click one of the icons if you don\'t believe!',
+        className: 'c2 r3',
+        component: <IphoneAnimation />,
+        direction: 'right'
+    }
+]
 
-            <GalleryCard tutorial="infinite-cards">
-                <InfiniteCards />
-            </GalleryCard>
+const IndexPage = () => {
+    const [hovering, setHovering] = useState(null);
 
-            <GalleryCard className="c3 r2">
-                <Text3d />
+    const renderTutorials = () => {
+        return tutorials.map((tut, i) => (
+            <GalleryCard
+                key={i}
+                tutorial={tut}
+                hovering={hovering === tut}
+                setHovering={setHovering}
+            >
+                {tut.component}
             </GalleryCard>
+        ));
+    }
 
-            <GalleryCard className="c2 r1">
-                Wave
-            </GalleryCard>
-
-            <GalleryCard className="c4 r1">
-                <UnderlinedMenu />
-            </GalleryCard>
+    return (
+        <div className="gallery">
+            <div className="gallery__info">
+                <h1>framer motion<br />playground</h1>
+                <button><Link to="/tutorial">about</Link></button>
+            </div>
             
-            <GalleryCard className="c2 r2">
-                <CustomCursor />
-            </GalleryCard>
+            <div className="gallery__container">
+                {renderTutorials()}
+            </div>
 
-            <GalleryCard className="c3 r2">
-                <ClipPathTransition />
-            </GalleryCard>
-
-            <GalleryCard className="c2 r3">
-                <RotatingCard />
-            </GalleryCard>
-
-            <GalleryCard className="c2 r3">
-                <IphoneAnimation />
-            </GalleryCard>
+            <motion.div 
+                className="gallery__dim-layer" 
+                style={{ height: use100vh() }} 
+                animate={{ opacity: hovering ? .8 : 0 }}
+                transition={{ duration: .15 }}
+            />
         </div>
-    </div>
-)
+    )
+}
 
 export default IndexPage
