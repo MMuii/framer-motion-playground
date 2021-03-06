73374e240a28ad3938ffec51154598934ed0c0af
Marcin Świderek <muimuikrol@gmail.com>
2021-03-06 01:52:36 +0100

    Testing delayed page transition

--- src/layouts/PageLayout.js
+++ src/layouts/PageLayout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { WindowSizeContext } from '../contexts/WindowSizeContext';
 import { useWindowSize } from '../hooks/useWindowSize';
@@ -16,12 +16,19 @@ const variants = {
     animate: {
         // clipPath: 'polygon(0 0, 100% 0%, 100% 100%, 0% 100%)'
         // clipPath: 'circle(70.7% at 50% 50%)'
-        clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'
+        clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+        transition: {
+            duration: .5,
+            delay: 1
+        }
     },
     exit: {
         // clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)'
         // clipPath: 'circle(0.0% at 50% 50%)'
-        clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)' //NIEWIDOCZNY PRAWO
+        clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)', //NIEWIDOCZNY PRAWO
+        transition: {
+            duration: .5
+        }
     }
 }
 
@@ -32,18 +39,18 @@ const PageLayout = ({ children, location }) => {
 
     return (
         <AnimatePresence initial={false} exitBeforeEnter>
-            {/* <motion.main 
+            <motion.main 
                 key={location.pathname}
                 variants={variants}
                 initial="initial"
                 animate="animate"
                 exit="exit"
-                transition={{ duration: .3 }}
-            > */}
+                // transition={{ duration: .5 }}
+            >
                 <WindowSizeContext.Provider value={{ height, width }}>
                     {children}
                 </WindowSizeContext.Provider>
-            {/* </motion.main> */}
+            </motion.main>
         </AnimatePresence>
     )
 }
