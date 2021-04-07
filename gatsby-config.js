8cb8ddb68a3d28873905dae1d7feb443110cf606
Marcin Świderek <muimuikrol@gmail.com>
2021-04-07 14:04:34 +0200

    Updated gatsby config and package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Framer Motion Playground`,
+    siteUrl: `https://www.framermotionplayground.com`,
+    description: `Interactive blog with Framer Motion tutorials`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
